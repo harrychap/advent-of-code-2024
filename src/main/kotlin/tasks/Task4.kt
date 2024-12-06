c6ce8272f5f0eebc7ebc1f99282891b0cec86cec
Harry Chaplain <harry.chaplain@flutter.com>
2024-12-06 15:23:02 +0000

    tidy

--- src/main/kotlin/tasks/Task4.kt
+++ src/main/kotlin/tasks/Task4.kt
@@ -5,9 +5,10 @@ import utils.readInput
 object Task04 : Task {
     private val pointMap = parseInput()
 
-    override fun partA(): Int {
-        var count = 0
-        pointMap.filter { it.value == 'X' }.forEach { point ->
+    override fun partA(): Int = pointMap
+        .filter { it.value == 'X' }
+        .map { point ->
+            var count = 0
             if (point.key.expandUp(3) == "MAS") ++count
             if (point.key.expandDown(3) == "MAS") ++count
             if (point.key.expandRight(3) == "MAS") ++count
@@ -16,25 +17,26 @@ object Task04 : Task {
             if (point.key.expandDownRight(3) == "MAS") ++count
             if (point.key.expandUpLeft(3) == "MAS") ++count
             if (point.key.expandUpRight(3) == "MAS") ++count
+            count
+        }.sum()
+
+
+    override fun partB(): Int = pointMap
+        .filter { it.value == 'A' }
+        .count { point ->
+            val leftBottom = "${point.key.expandDownLeft(1)}A${point.key.expandUpRight(1)}"
+            val rightBottom = "${point.key.expandDownRight(1)}A${point.key.expandUpLeft(1)}"
+            val rightTop = "${point.key.expandUpRight(1)}A${point.key.expandDownLeft(1)}"
+            val leftTop = "${point.key.expandUpLeft(1)}A${point.key.expandDownRight(1)}"
+            (leftBottom == "MAS" && rightBottom == "MAS") || (leftBottom == "MAS" && leftTop == "MAS") || (rightBottom == "MAS" && rightTop == "MAS") || (leftTop == "MAS" && rightTop == "MAS")
         }
-        return count
-    }
-
-    override fun partB(): Int = pointMap.filter { it.value == 'A' }.count { point ->
-        val leftBottom = "${point.key.expandDownLeft(1)}A${point.key.expandUpRight(1)}"
-        val rightBottom = "${point.key.expandDownRight(1)}A${point.key.expandUpLeft(1)}"
-        val rightTop = "${point.key.expandUpRight(1)}A${point.key.expandDownLeft(1)}"
-        val leftTop = "${point.key.expandUpLeft(1)}A${point.key.expandDownRight(1)}"
-        (leftBottom == "MAS" && rightBottom == "MAS") || (leftBottom == "MAS" && leftTop == "MAS") || (rightBottom == "MAS" && rightTop == "MAS") || (leftTop == "MAS" && rightTop == "MAS")
-
-    }
 
     private fun parseInput() =
         readInput("task04.txt").split("\n").flatMapIndexed { y, row ->
             row.mapIndexed { x, char ->
                 Pair(x, y) to char
             }
-        }.associateBy({it.first}, {it.second})
+        }.associateBy({ it.first }, { it.second })
 
     private fun Pair<Int, Int>.expandRight(expandSize: Int): String =
         (1..expandSize).joinToString("") { size -> "${pointMap[Pair(this.first + size, this.second)]}" }
